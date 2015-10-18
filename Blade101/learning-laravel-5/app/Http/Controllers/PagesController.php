b03b793d936a2c437e2d0cee4a6c6449181772e9
adam Alvarado <adamalvarado@iesebre.com>
2015-10-18 17:23:40 +0200

    View contact and modifying contact adding the section

--- Blade101/learning-laravel-5/app/Http/Controllers/PagesController.php
+++ Blade101/learning-laravel-5/app/Http/Controllers/PagesController.php
@@ -18,4 +18,9 @@ class PagesController extends Controller
 
         return view('pages.about', $data);
     }
+
+    public function contact()
+    {
+        return view('pages.contact');
+    }
 }
--- Blade101/learning-laravel-5/resources/views/pages/contact.blade.php
+++ Blade101/learning-laravel-5/resources/views/pages/contact.blade.php
@@ -1,13 +1,5 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <tittle>Adam</tittle>
-    </head>
+@extends('app)
 
-    <body>
-
+@section('content')
         <h1>Contact Me!</h1>
-
-    </body>
-</html>
+@stop
